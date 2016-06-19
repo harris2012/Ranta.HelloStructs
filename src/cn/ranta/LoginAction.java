e621ccf9916a2a8291b7465f99ba5767ea2249d7
HarrisZhang <harriszhang@live.cn>
2016-06-19 13:07:30 +0800

    init project

---
+++ src/cn/ranta/LoginAction.java
@@ -0,0 +1,13 @@
+package cn.ranta;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LoginAction extends ActionSupport {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public String execute() throws Exception {
+        return SUCCESS;
+    }
+}
